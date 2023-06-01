2693c3a698b5b7c967422a2a4602b5b3630035e1
Colson <colsoncalifornication@gmail.com>
2023-06-01 10:42:12 +0545

    render slected movie stats

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ const onInput = async (event) => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
-    option.addEventListener('click', () =>{
+    option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
       onMovieSelect(movie);
-    })
+    });
 
     resultsWrapper.appendChild(option);
   }
@@ -60,11 +60,11 @@ const onInput = async (event) => {
 
 input.addEventListener('input', debounce(onInput, 500));
 
-document.addEventListener('click', event => {
+document.addEventListener('click', (event) => {
   if (!root.contains(event.target)) {
     dropdown.classList.remove('is-active');
   }
-})
+});
 
 const onMovieSelect = async (movie) => {
   const apiKey = await import('./apiKey.js').then((module) => module.apiKey);
@@ -76,4 +76,62 @@ const onMovieSelect = async (movie) => {
   });
 
   console.log(response.data);
-}
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>Type: ${movieDetail.Type.toUpperCase()}</h4>
+          <h4>Genre: ${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.Released}</p>
+      <p class="subtitle">Released</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.Actors}</p>
+      <p class="subtitle">Actors</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.Director}</p>
+      <p class="subtitle">Director</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+    <article class="notification is-primary article-background">
+      <p class="title">${movieDetail.Runtime}</p>
+      <p class="subtitle">Movie Length</p>
+    </article>
+  `;
+};
