082808d3bf765c890378db2bea287ff9fece00fe
Colson <colsoncalifornication@gmail.com>
2023-06-01 08:05:09 +0545

    add search dropdown menu

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-const fetchData = async () => {
+const fetchData = async (searchQuery) => {
   const apiKey = await import('./apiKey.js').then((module) => module.apiKey);
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: apiKey,
-      s: 'iron man',
+      s: searchQuery,
     },
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
-fetchData();
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  dropdown.classList.add('is-active');
+  for (let movie of movies) {
+    const option = document.createElement('a');
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${movie.Poster}" />
+      ${movie.Title}
+    `;
+
+    resultsWrapper.appendChild(option);
+  }
+};
+
+input.addEventListener('input', debounce(onInput, 500));
